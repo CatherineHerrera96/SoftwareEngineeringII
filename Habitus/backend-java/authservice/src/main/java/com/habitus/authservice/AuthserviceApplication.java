69d0fac9bae17fe2a5645ac329240c82b69cc81b
ezcles <82733195+Baruj-Ramirez@users.noreply.github.com>
2025-11-30 16:58:09 -0500

    added . env integration for password security

--- Habitus/backend-java/authservice/src/main/java/com/habitus/authservice/AuthserviceApplication.java
+++ Habitus/backend-java/authservice/src/main/java/com/habitus/authservice/AuthserviceApplication.java
@@ -1,13 +1,22 @@
 package com.habitus.authservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        // Load .env variables before Spring starts
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(AuthserviceApplication.class, args);
+    }
 }
+
